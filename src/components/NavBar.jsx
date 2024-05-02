3eb98145f7bd35e92af94d804755e26687495e6f
Denis Sab <denis.sabs@ymail.com>
2024-05-02 17:40:41 +0200

    navbar first

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import "../index.css"
+
+function NavBar () {
+
+    return (
+
+        <div id="div-nav">
+            <div id="div-logo">
+                <p >LOGO</p>
+            </div>
+
+            <div id="div-search">
+                <input type="text"  placeholder="Search Topic"/>
+            </div>
+        </div>
+        
+    )
+}
+
+export default NavBar;
