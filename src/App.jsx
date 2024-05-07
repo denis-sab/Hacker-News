d90e6ca56176817e790f73fdbabd7e89e3495d45
denis-sab <denis.sabs@ymail.com>
2024-05-07 16:15:08 +0200

    "test"

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
 import { useState } from 'react';
 import NavBar from "./components/NavBar";
 import ResultList from "./components/ResultList";
 import Footer from './components/Footer';
 
-=======
-import { Component, useState } from 'react'
->>>>>>> dec581ce60d31c8c9848c86b9239b5dbbbc4e2b4
 import './App.css'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 
-<<<<<<< HEAD
 export default function App(){
 
   const [searchQuery, setSearchQuery]=useState("");
@@ -22,16 +17,5 @@ export default function App(){
       <ResultList searchQuery={searchQuery} />
       <Footer />
     </div>
-=======
-function App() {
- 
-  
-
-  return (
-    <>
-    <NavBar />
-    <Footer />
-    </>
->>>>>>> dec581ce60d31c8c9848c86b9239b5dbbbc4e2b4
   )
 }
